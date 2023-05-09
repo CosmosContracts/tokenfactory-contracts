9c9b87cccf4d68aa615f3108ab94c0b7b12ff272
Reece Williams <reecepbcups@gmail.com>
2023-05-09 13:29:22 -0500

    CW20Message

--- test/helpers/cosmwasm.go
+++ test/helpers/cosmwasm.go
@@ -2,9 +2,15 @@ package test
 
 import (
 	"context"
+	b64 "encoding/base64"
+	"fmt"
 	"testing"
 
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	"github.com/strangelove-ventures/interchaintest/v4/chain/cosmos"
+	"github.com/strangelove-ventures/interchaintest/v4/ibc"
+	"github.com/strangelove-ventures/interchaintest/v4/testutil"
+	"github.com/stretchr/testify/require"
 )
 
 func SetupContract(t *testing.T, ctx context.Context, chain *cosmos.CosmosChain, keyname string, fileLoc string, message string) (codeId, contract string) {
@@ -23,7 +29,29 @@ func SetupContract(t *testing.T, ctx context.Context, chain *cosmos.CosmosChain,
 	return codeId, contractAddr
 }
 
-// func CW20Message(t *testing.T, ctx context.Context, chain *cosmos.CosmosChain, user *ibc.Wallet, cw20ContractAddr, actionContractAddr, amount, message string) {
-// 	msg := fmt.Sprintf(`{"send":{"contract":"%s","amount":"%s","msg":"%s"}}`, actionContractAddr, amount, b64.StdEncoding.EncodeToString([]byte(message)))
-// 	txHash, _ := chain.ExecuteContract(ctx, user.KeyName, cw20ContractAddr, msg)
-// }
+func CW20Message(t *testing.T, ctx context.Context, chain *cosmos.CosmosChain, user *ibc.Wallet, cw20ContractAddr, actionContractAddr, amount, message string) {
+	msg := fmt.Sprintf(`{"send":{"contract":"%s","amount":"%s","msg":"%s"}}`, actionContractAddr, amount, b64.StdEncoding.EncodeToString([]byte(message)))
+
+	// not enough gas... used 200k but needs more. So doing manually.
+	// txHash, _ := chain.ExecuteContract(ctx, user.KeyName, cw20ContractAddr, msg)
+
+	cmd := []string{"junod", "tx", "wasm", "execute", cw20ContractAddr, msg,
+		"--node", chain.GetRPCAddress(),
+		"--home", chain.HomeDir(),
+		"--chain-id", chain.Config().ChainID,
+		"--from", user.KeyName,
+		"--gas", "500000",
+		"--keyring-dir", chain.HomeDir(),
+		"--keyring-backend", keyring.BackendTest,
+		"-y",
+	}
+	_, _, err := chain.Exec(ctx, cmd, nil)
+	require.NoError(t, err)
+
+	// print stdout
+	// t.Log("CW20Message", string(stdout))
+
+	if err := testutil.WaitForBlocks(ctx, 2, chain); err != nil {
+		t.Fatal(err)
+	}
+}
