285491594184481a8aa40a0d27820760713461c4
Reece Williams <reecepbcups@gmail.com>
2023-05-09 13:30:12 -0500

    Remove old comments

--- test/interchaintest/cw20_conversion_test.go
+++ test/interchaintest/cw20_conversion_test.go
@@ -68,17 +68,10 @@ func TestCw20ConversionMigrateContract(t *testing.T) {
 	// txHash, _ := juno.ExecuteContract(ctx, user.KeyName, cw20ContractAddr, msg)
 	helpers.CW20Message(t, ctx, juno, user, cw20ContractAddr, cw20MigrateContractAddr, "5", `{"receive":{}}`)
 
-	// t.Log(txHash)
-
-	// t.Log("GetHostRPCAddress", juno.GetHostRPCAddress())
-	// testutil.WaitForBlocks(ctx, 20_000, juno)
-
 	// we should now have 5 balance of the tf denom
 	CheckBalance(t, ctx, juno, uaddr, tfDenom, 5)
 	// the cw20 migrate contract should still have 0 balance of this denom (to ensure it does not double mint)
 	CheckBalance(t, ctx, juno, cw20MigrateContractAddr, tfDenom, 0)
-
-	// TODO: native migrate here upload_nativemigrate
 
 	// !important: debugging
 	// t.Log("GetHostRPCAddress", juno.GetHostRPCAddress())
@@ -89,32 +82,3 @@ func TestCw20ConversionMigrateContract(t *testing.T) {
 		_ = ic.Close()
 	})
 }
-
-// NEW CONTRACTS HERE:
-// cw20Msg := fmt.Sprintf(`{"name":"test","symbol":"aaaa","decimals":6,"initial_balances":[{"address":"%s","amount":"100"}]}`, uaddr)
-// _, cw20ContractAddr := helpers.SetupContract(t, ctx, juno, user.KeyName, "../../base_artifacts/cw20_base.wasm", cw20Msg)
-
-// Setup the migration contract (convert a cw20 to a native denom (ex: ibc, native, or factory))
-// migrateCW20Msg := fmt.Sprintf(`{"cw20_token_address":"%s","contract_minter_address":"%s","tf_denom":"%s"}`, cw20ContractAddr, tfCoreContractAddr, tfDenom)
-// _, cw20MigrateContractAddr := helpers.SetupContract(t, ctx, juno, user.KeyName, "../../artifacts/migrate.wasm", migrateCW20Msg)
-
-// // Allow the Migration contract to mint through the Tokenfactory Core contract
-// msg := fmt.Sprintf(`{"add_whitelist":{"addresses":["%s"]}}`, cw20MigrateContractAddr)
-// if _, err := juno.ExecuteContract(ctx, user.KeyName, tfCoreContractAddr, msg); err != nil {
-// 	t.Fatal(err)
-// }
-
-// // Ensure the contract config data is set correctly.
-// res := GetContractConfig(t, ctx, juno, tfCoreContractAddr, uaddr)
-// // t.Log("v.Data", v.Data)
-// assert.Equal(t, res.Data.AllowedMintAddresses[0], cw20MigrateContractAddr)
-// assert.Equal(t, res.Data.Denoms[0], tfDenom)
-
-// // remove whitelist and ensure no one can mint
-// msg = fmt.Sprintf(`{"remove_whitelist":{"addresses":["%s"]}}`, cw20MigrateContractAddr)
-// if _, err := juno.ExecuteContract(ctx, user.KeyName, tfCoreContractAddr, msg); err != nil {
-// 	t.Fatal(err)
-// }
-// // get config and ensure they are gone
-// res = GetContractConfig(t, ctx, juno, tfCoreContractAddr, uaddr)
-// assert.Equal(t, len(res.Data.AllowedMintAddresses), 0)
