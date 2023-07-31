d5aab6e656b5003dbf825d1014ebc42d33014367
Reece Williams <reecepbcups@gmail.com>
2023-07-31 16:01:59 -0500

    mainnet and testnet upload

--- scripts/testnet_upload.sh
+++ scripts/testnet_upload.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# Uploads the contracts to Mainnet & returns their code_ids
+# Uploads the contracts & returns their code_ids
 #
-# Usage: sh scripts/mainnet_upload.sh
+# Usage: sh scripts/testnet_upload.sh
 #
 
-KEY="reece-main"
-CHAINID="juno-1"
+KEY="validator"
+CHAINID="uni-6"
 JUNOD_HOME="$HOME/.juno"
 KEYALGO="secp256k1"
 KEYRING="os"
 
-export JUNOD_NODE="https://rpc.juno.strange.love:443"
-export JUNOD_COMMAND_ARGS="--gas=auto -y --from $KEY --broadcast-mode block --output json --chain-id=$CHAINID --gas-prices=0.075ujuno --gas-adjustment=1.3 --home "$JUNOD_HOME" --keyring-backend $KEYRING"
+export JUNOD_NODE="https://uni-rpc.reece.sh:443"
+export JUNOD_COMMAND_ARGS="--gas=auto -y --from $KEY --broadcast-mode=sync --output json --chain-id=$CHAINID --gas-prices=0.003ujunox --gas-adjustment=1.5 --home "$JUNOD_HOME" --keyring-backend $KEYRING"
 
 middleware_tx=$(junod tx wasm store artifacts/tokenfactory_core.wasm $JUNOD_COMMAND_ARGS | jq -r .txhash)
 middleware_code_id=$(junod q tx $middleware_tx --output=json | jq -r '.logs[0].events[] | select(.type=="store_code")' | jq -r .attributes[1].value) && echo "Middleware code_id: $middleware_code_id"
